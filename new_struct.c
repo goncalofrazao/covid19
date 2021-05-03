31ee49286b8d0d5df447874bb78e38a6237f2cc8
Gonçalo Frazão <gonfrazao@gmail.com>
2021-05-03 19:43:22 +0100

    separei as funcoes para criar estrutura num novo ficheiro

---
+++ new_struct.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "data.h"
+#include "structures.h"
+
+fix_t *create_new_fix(char *country, char *initials, char *continent, long population){
+    fix_t *result = (fix_t*) malloc(sizeof(fix_t));
+    strcpy(result->country, country);
+    strcpy(result->initials, initials);
+    strcpy(result->continent, continent);
+    result->population = population;
+    result->next = NULL;
+    return result;
+}
+
+var_t *create_new_var(int week, int year, char *indicator, int weekly_count, float rate_14_day, int cumulative_count){
+    var_t *result = (var_t*) malloc(sizeof(var_t));
+    result->week = week;
+    result->year = year;
+    if(strcmp(indicator, "deaths") == 0){
+        result->weekly_deads = weekly_count;
+        result->racio_deaths = rate_14_day;
+        result->cumulative_deaths = cumulative_count;
+    }else{
+        result->weekly_cases = weekly_count;
+        result->racio_cases = rate_14_day;
+        result->cumulative_cases = cumulative_count;
+    }
+    result->next = NULL;
+    return result;
+}
+
+fix_t *insert_at_head(fix_t *head, fix_t *fix_to_insert){
+    fix_to_insert->next = head;
+    return fix_to_insert;
+}
+
+var_t *insert_at_head_var(var_t *head, var_t *var_to_insert){
+    var_to_insert->next = head;
+    return var_to_insert;
+}
---
+++ new_struct.h
@@ -0,0 +1,11 @@
+#include "structures.h"
+
+#ifndef _NEW_STRUCT_
+#define _NEW_STRUCT_
+
+fix_t *create_new_fix(char *country, char *initials, char *continent, long population);
+var_t *create_new_var(int week, int year, char *indicator, int weekly_count, float rate_14_day, int cumulative_count);
+fix_t *insert_at_head(fix_t *head, fix_t *fix_to_insert);
+var_t *insert_at_head_var(var_t *head, var_t *var_to_insert);
+
+#endif
