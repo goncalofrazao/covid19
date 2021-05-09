1c021b45d3a3a3d616190e00956a5a747d091629
Gonçalo Frazão <gonfrazao@gmail.com>
2021-05-09 23:28:29 +0100

    added some comments

--- restrict_data.c
+++ restrict_data.c
@@ -5,6 +5,26 @@
 #include "restrict_data.h"
 #include "structures.h"
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Funtion name: remove_struct_fix
+ * 
+ * Arguments: fix_t *to_remove -- pointer to list element to delete
+ *            fix_t **head -- double pointer to head of the list
+ * 
+ * Return: none
+ * 
+ * Side-effects: - libert allocated memory for the list element to delete
+ * 
+ *               - if there is previous changes the 'next' of the element
+ * before to the next of the element to delete, else puts head in next
+ * 
+ *               - changes the 'previous' of the next element to the
+ * previous element or to NULL if there is no previous
+ * 
+ * Description: This function deletes a element of a list of type fix
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 void remove_struct_fix(fix_t *to_remove, fix_t **head)
 {
     if (to_remove->previous != NULL)
--- structures.h
+++ structures.h
@@ -1,30 +1,30 @@
 #ifndef _STRUCTURES_
 #define _STRUCTURES_
 
-typedef struct fix
+typedef struct fix //linked list with country data
 {
-    char country[30];
-    char initials[4];
-    char continent[10];
-    long population;
-    struct var *var;
-    struct fix *next;
-    struct fix *previous;
+    char country[30];     //name of the country
+    char initials[4];     //initials of the country
+    char continent[10];   //continent of the country
+    long population;      //population of the country
+    struct var *var;      //pointer to the head of the list with weekly data
+    struct fix *next;     //pointer to next element of the list
+    struct fix *previous; //pointer to the previous element of the list
 } fix_t;
 //typedef struct fix fix_t;
 
-typedef struct var
+typedef struct var //linked list with weekly data
 {
-    int week;
-    int year;
-    int weekly_cases;
-    int weekly_deaths;
-    int racio_cases;
-    int racio_deaths;
-    int cumulative_cases;
-    int cumulative_deaths;
-    struct var *next;
-    struct var *previous;
+    int week;              //week
+    int year;              //year
+    int weekly_cases;      //cases of the week
+    int weekly_deaths;     //deaths of the week
+    int racio_cases;       //ratio of cases per 100k habitants
+    int racio_deaths;      //ratio of deaths per 1M habitants
+    int cumulative_cases;  //cumulative cases of the country
+    int cumulative_deaths; //cumulative deaths of the country
+    struct var *next;      //pointer to next list element
+    struct var *previous;  //pointer to previous list element
 } var_t;
 //typedef struct var var_t;
 
