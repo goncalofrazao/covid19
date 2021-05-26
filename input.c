10330b8f745b8fc32e720b87ec1c7b0f2266a857
jc <63022203+jcjoao@users.noreply.github.com>
2021-05-26 23:05:24 +0100

    comentarios

--- input.c
+++ input.c
@@ -50,6 +50,7 @@ fix_t *read_input(char *what_to_read, char *filename)
         printf("Erro na abertura do ficheiro");
         exit(EXIT_FAILURE);
     }
+    //scan first line to discard it (we dont need it since it's always the same)
     fscanf(fp, "%*[^\n]\n");
 
     while(fgets(str, MAXLEN, fp) != NULL){
@@ -70,34 +71,38 @@ fix_t *read_input(char *what_to_read, char *filename)
         while((token = strtok(NULL,",")) != NULL && i < 9){
             column[i++] = token;
         }
+        //less than 8 columns is an error
         if(i < 8){
             read_error();
         }
 
+        //check errors
         for(int j = 0; j < i; j++){
             switch (j)
             {
+			//for columns 0,1,2,4 we need to check the string
             case 0:
             case 1:
             case 2:
             case 4:
                 check_string(column[j]);
                 break;
-
+			//for columns 0,1,2,4 we need to check the int
             case 3:
             case 5:
             case 8:
                 check_int(column[j]);
                 break;
-
+			//for columns 6 we need to check if the date is in the right format
             case 6:
                 check_date(column[j]);
                 break;
-
             case 7:
+				//if it only has 8 columns, check int (cumulative count)
                 if(i == 8){
                     check_int(column[j]);
                 }
+                //if it has 9 columns, check float (rate_14_day)
                 else{
                     check_float(column[j]);
                 }
@@ -117,9 +122,10 @@ fix_t *read_input(char *what_to_read, char *filename)
             strcpy(indicator, column[4]);
             weekly_count = atoi(column[5]);
             sscanf(column[6], "%d-%d", &year, &week);
+            //there are no years with 54 weeks!
             if(week > 53)
                 read_error();
-
+			//sometimes there is no data for rate_14_day, in those cases strtok only creates 8 tokens!
             if(i == 8){
                 cumulative_count = atoi(column[7]);
                 rate_14_day = 0;
@@ -128,20 +134,24 @@ fix_t *read_input(char *what_to_read, char *filename)
                 rate_14_day = atof(column[7]);
                 cumulative_count = atoi(column[8]);
             }
-
             aux1 = find_country(head, country);
+            //if a structure for the country doesnt exist yet, we create one
             if(aux1 == NULL){
                 aux1 = create_country(country, initials, continent, population);
                 head = insert_fix(aux1, head);
                 aux2 = create_date(year, week, indicator, weekly_count, rate_14_day, cumulative_count);
                 aux1->var = insert_var(aux2, aux1->var);
             }
+            //if there is already a structure for the country, we update that structure
             else{
+				//check if date already exists
                 aux2 = find_date(aux1->var, year, week);
+                //if it doesnt, create a new date
                 if(aux2 == NULL){
                     aux2 = create_date(year, week, indicator, weekly_count, rate_14_day, cumulative_count);
                     aux1->var = insert_var(aux2, aux1->var);
                 }
+                //if it does, update the data for the date
                 else{
                     update_date(aux2, indicator, weekly_count, rate_14_day, cumulative_count);
                 }
@@ -169,15 +179,19 @@ void output(fix_t *head, char *filename)
     fix_t *aux1;
     var_t *aux2;
     FILE *fp = fopen(filename, "w");
+    //write the first line that indicates the columns
     fprintf(fp, "country,country_code,continent,population,indicator,weekly_count,year_week,rate_14_day,cumulative_count\n");
+    //go through the list of structures
     while(head != NULL){
         while(head->var != NULL){
             if(head->var->cases == 1)
                 fprintf(fp, "%s, %s, %s, %lu, cases, %d, %4d-%2d, %f, %d\n", head->name, head->initials, head->continent, head->population, head->var->weekly_cases, head->var->year, head->var->week, head->var->rate_cases, head->var->cumulative_cases);
             if(head->var->deaths == 1)
                 fprintf(fp, "%s, %s, %s, %lu, deaths, %d, %4d-%2d, %f, %d\n", head->name, head->initials, head->continent, head->population, head->var->weekly_deaths, head->var->year, head->var->week, head->var->rate_deaths, head->var->cumulative_deaths);
+            //save the next struct on aux2
             aux2 = head->var->next;
             free(head->var);
+            //head->var becomes the next struct
             head->var = aux2;
         }
         aux1 = head->next;
@@ -202,16 +216,21 @@ void output(fix_t *head, char *filename)
 void binary_output(fix_t *head, char *filename)
 {
     FILE *fp = fopen(filename, "wb");
+    //count number of fix structures
     int i = count_fix(head);
     var_t *aux1;
     fix_t *aux2;
     fwrite(&i, sizeof(int), 1, fp);
     while(head != NULL){
+		//write the fix structure
         fwrite(head, sizeof(fix_t), 1, fp);
+        //count number of var structures
         i = count_var(head->var);
         fwrite(&i, sizeof(int), 1, fp);
         for(int a = 0; a < i; a++){
+			//write the var structure
             fwrite(head->var, sizeof(var_t), 1, fp);
+            //save next on aux1
             aux1 = head->var->next;
             free(head->var);
             head->var = aux1;
@@ -242,18 +261,24 @@ fix_t *binary_input(char *filename)
     var_t *aux1;
     fix_t *aux2;
     head = NULL;
+    //read number of fix structures
     fread(&i, sizeof(int), 1, fp);
+    //go through all of the fix structures written on the .dat file
     for(int b = 0; b < i; b++){
         aux2 = (fix_t*) malloc(sizeof(fix_t));
         fread(aux2, sizeof(fix_t), 1, fp);
         aux2->next = NULL;
         aux2->var = NULL;
+        //insert structure on the list
         head = insert_fix(aux2, head);
+        //read number of var structures
         fread(&j, sizeof(int), 1, fp);
+        //go through all of the var structures
         for(int a = 0; a < j; a++){
             aux1 = (var_t*) malloc(sizeof(var_t));
             fread(aux1, sizeof(var_t), 1, fp);
             aux1->next = NULL;
+            //insert var structure
             aux2->var = insert_var(aux1, aux2->var);
         }
     }
@@ -274,8 +299,10 @@ fix_t *binary_input(char *filename)
 int count_var(var_t *head)
 {
     int i = 0;
+    //go thought the list counting the number of var structures, until the end is reached
     while(head != NULL){
         i++;
+        //go to the next structure
         head = head->next;
     }
     return i;
@@ -294,8 +321,10 @@ int count_var(var_t *head)
 int count_fix(fix_t *head)
 {
     int i = 0;
+    //go thought the list counting the number of fix structures, until the end is reached
     while(head != NULL){
         i++;
+        //go to the next structure
         head = head->next;
     }
     return i;
@@ -360,6 +389,7 @@ void check_float(char *check)
         if(check[i] == '.')
             flag++;
     }
+    //floats only have one '.', more than that is an error
     if(flag > 1)
         read_error();
 }
@@ -383,6 +413,7 @@ void check_line(char *line)
             comma_number++;
         line++;
     }
+    //every line on the .csv file has 8 commas
     if(comma_number < 8)
         read_error();
 }
@@ -407,7 +438,8 @@ void check_date(char *date)
         if((date[i] < '0' || date[i] > '9') && !(date[i] == '-'))
             read_error();
     }
-    if(ifen_number > 1)
+    //dates are on the format year-week, so they only have one ifen
+    if(ifen_number > 1 || ifen_number == 0)
         read_error();
 }
 
