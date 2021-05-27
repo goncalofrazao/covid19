e6912f19da5401a29da7a49b3424f0b81a69fb72
Gonçalo Frazão <gonfrazao@gmail.com>
2021-05-27 22:13:17 +0100

    console commented

--- console.c
+++ console.c
@@ -6,37 +6,113 @@
 #include "console.h"
 #include "structures.h"
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_input_file_name
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *
+ * Return: argv[i + 1] -- pointer to first element of string after '-i'
+ *         NULL -- just to avoid warning (program ends with console_error)
+ *
+ * Description: This function identifies the pointer to the input filename
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 char *get_input_file_name(int argc, char **argv)
 {
+    // search '-i'
     for(int i = 1; i < argc; i++){
+        // return pointer to string after '-i'
         if(argv[i][0] == '-' && argv[i][1] == 'i' && strlen(argv[i]) == 2)
             return argv[i + 1];
     }
+    // '-o' is a must in argv
+    console_error();
     return NULL;
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_output_file_name
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *
+ * Return: argv[i + 1] -- pointer to first element of string after '-o'
+ *         NULL -- just to avoid warning (program ends with console_error)
+ *
+ * Description: This function identifies the pointer to the output filename
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 char *get_ouput_file_name(int argc, char **argv)
 {
+    // search '-o'
     for(int i = 1; i < argc; i++){
+        // return pointer to string after '-o'
         if(argv[i][0] == '-' && argv[i][1] == 'o' && strlen(argv[i]) == 2)
             return argv[i + 1];
     }
+    // '-o' is a must in argv
+    console_error();
     return NULL;
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_L
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *
+ * Return: argv[i + 1] -- pointer to first element of string after -L          
+ *         NULL -- if there is no '-L'
+ *
+ * Description: This function identifies the pointer to the string after -L
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 char *get_L(int argc, char **argv)
 {
+    // search '-L'
     for(int i = 1; i < argc; i++){
-        if(argv[i][0] == '-' && argv[i][1] == 'L' && strlen(argv[i]) == 2)
+        // return pointer to string after '-L'
+        if(argv[i][0] == '-' && argv[i][1] == 'L' && strlen(argv[i]) == 2){
+            // check if continent is valid
+            if(strmp(argv[i + 1], "all") != 0 && strmp(argv[i + 1], "Europe") != 0 && strmp(argv[i + 1], "Africa") != 0 && strmp(argv[i + 1], "Asia") != 0 && strmp(argv[i + 1], "Oceania") != 0 && strmp(argv[i + 1], "America") != 0){
+                printf("INVALID CONTINENT\n");
+                console_error();
+            }
             return argv[i + 1];
+        }
     }
+    // if there is no '-L'
     return NULL;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_S
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *            int *year -- year to get
+ *            int *week -- week to get
+ *
+ * Return: DEA -- if option is dea
+ *         INF -- if option is inf
+ *         ALFA -- if option is alfa or if there is no '-S' in argv
+ *         POP -- if option is pop
+ *
+ * Description: This function gets the S option and checks if it is
+ *              valid
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 order_data get_S(int argc, char **argv, int *year, int *week)
 {
+    // search '-S'
     for(int i = 1; i < argc; i++){
         if(argv[i][0] == '-' && argv[i][1] == 'S' && strlen(argv[i]) == 2){
+            // check option of -S and get week if it is the case
             if(strcmp(argv[i + 1], "dea") == 0){
                 sscanf(argv[i + 2], "%d-%d", year, week);
                 return DEA;
@@ -51,19 +127,42 @@ order_data get_S(int argc, char **argv, int *year, int *week)
             else if(strcmp(argv[i + 1], "pop") == 0){
                 return POP;
             }
+            // return error if no valid input
             else{
                 printf("S argument invalid\n");
                 console_error();
             }
         }
     }
+    // default option if there is no -S
     return ALFA;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_D
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *            int *year -- year to get
+ *            int *week -- week to get
+ *
+ * Return: SELECT_DEA -- if option is selectdea
+ *         SELECT_INF -- if option is selectinf
+ *         RACIOINF -- if option is racioinf
+ *         RACIODEA -- if option is raciodea
+ *         NO_INPUT_1 -- if there is no D option
+ *
+ * Description: This function gets the D option and checks if it is
+ *              valid
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 select_data get_D(int argc, char **argv)
 {
+    // search D option
     for(int i = 1; i < argc; i++){
         if(argv[i][0] == '-' && argv[i][1] == 'D' && strlen(argv[i]) == 2){
+            // check option
             if(strcmp(argv[i + 1], "inf") == 0)
                 return SELECT_INF;
             else if(strcmp(argv[i + 1], "dea") == 0)
@@ -72,19 +171,44 @@ select_data get_D(int argc, char **argv)
                 return RACIOINF;
             else if(strcmp(argv[i + 1], "raciodea") == 0)
                 return RACIODEA;
+            // end if no valid option 
             else{
                 printf("D argument invalid\n");
                 console_error();
             }
         }
     }
+    // no D option in argv
     return NO_INPUT_1;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: get_P
+ *
+ * Arguments: int argc -- number of arguments
+ *            char **argv -- arguments of command line
+ *            int *n -- pointer to number of habitants to restrict
+ *            int *year -- pointer to low limit date (year) to restrict
+ *            int *week -- pointer to low limit date (week) to restrict
+ *            int *max_year -- pointer to up limit date (year) to restrict
+ *            int *max_week -- pointer to up limit date (week) to restrict
+ *
+ * Return: MIN -- if option is min
+ *         MAX -- if option is max
+ *         DATE -- if option is date
+ *         DATES -- if option is dates
+ *         NO_INPUT_2 -- if there is no P option
+ *
+ * Description: This function gets the P option and checks if it is
+ *              valid
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 restrict_data get_P(int argc, char **argv, int *n, int *year, int *week, int *max_year, int *max_week)
 {
+    // search '-P'
     for(int i = 1; i < argc; i++){
         if(argv[i][0] == '-' && argv[i][1] == 'P' && strlen(argv[i]) == 2){
+            // validate option and scan date or n if that is the case
             if(strcmp(argv[i + 1], "min") == 0){
                 (*n) = atoi(argv[i + 2]);
                 return MIN;
@@ -104,35 +228,82 @@ restrict_data get_P(int argc, char **argv, int *n, int *year, int *week, int *ma
                     change_dates(year, week, max_year, max_week);
                 return DATES;
             }
+            // if invalid option
             else{
                 printf("P argument invalid\n");
                 console_error();
             }
         }
     }
+    // if there is no input of 'P'
     return NO_INPUT_2;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: check_dates
+ *
+ * Arguments: int year1 -- year of date 1
+ *            int week1 -- week of date 1
+ *            int year2 -- year of date 2
+ *            int week2 -- week of date 2
+ *
+ * Return: -1 -- if date 1 is later than date 2
+ *          1 -- if date 1 is earlier than date 2
+ * 
+ * Description: This function checks if date 1 is earlier than date 2
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 int check_dates(int year1, int week1, int year2, int week2)
 {
+    // check year
     if(year1 > year2)
         return -1;
+    // if year is the same, check week
     else if(year1 == year2 && week1 > week2)
         return -1;
     else
         return 1;
 }
 
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: change_dates
+ *
+ * Arguments: int *year1 -- pointer to year of date 1
+ *            int *week1 -- pointer to week of date 1
+ *            int *year2 -- pointer to year of date 2
+ *            int *week2 -- pointer to week of date 2
+ *
+ * Return: no return
+ * 
+ * Description: This function changes 2 dates
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 void change_dates(int *year1, int *week1, int *year2, int *week2)
 {
+    // save year 1 in aux
     int aux;
     aux = (*year1);
+    // put year 2 in year 1
     (*year1) = (*year2);
+    // put year 1 in year 2
     (*year2) = aux;
+    // save week 1 in aux
     aux = (*week1);
+    // put week 2 in week 1
     (*week1) = (*week2);
+    // put week 1 in week 2
     (*week2) = aux;
 }
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Function name: change_dates
+ *
+ * Arguments: no arguments
+ *
+ * Return: no return
+ * 
+ * Description: This function prints help msg and exit program
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 void console_error()
 {
